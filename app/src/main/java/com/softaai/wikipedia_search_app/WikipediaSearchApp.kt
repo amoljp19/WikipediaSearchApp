514a7e29f0bd6024333195a0c55fc471583591fe
amol pawar <amoljp19@gmail.com>
2021-02-04 20:15:23 +0530

    added added module, application classes and internet permission

---
+++ app/src/main/java/com/softaai/wikipedia_search_app/WikipediaSearchApp.kt
@@ -0,0 +1,8 @@
+package com.softaai.wikipedia_search_app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WikipediaSearchApp : Application() {
+}
--- app/src/main/java/com/softaai/wikipedia_search_app/utils/Constants.kt
+++ app/src/main/java/com/softaai/wikipedia_search_app/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.softaai.wikipedia_search_app.utils
 
 class Constants {
     companion object {
-        const val URL: String = "https://en.wikipedia.org"
+        const val BASE_URL: String = "https://en.wikipedia.org"
     }
 }
