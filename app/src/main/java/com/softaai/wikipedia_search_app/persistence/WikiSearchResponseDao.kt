8963bf4021c01f988ea33f739a6af7bda4a4ffc1
amol pawar <amoljp19@gmail.com>
2021-02-05 11:14:09 +0530

    added Dao interface and its methods

---
+++ app/src/main/java/com/softaai/wikipedia_search_app/persistence/WikiSearchResponseDao.kt
@@ -0,0 +1,20 @@
+package com.softaai.wikipedia_search_app.persistence
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.softaai.wikipedia_search_app.model.Page
+
+@Dao
+interface WikiSearchResponseDao {
+    
+    @Query("SELECT * FROM Page")
+    fun getAllWikiPages(): List<Page>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(page: Page)
+
+    @Query("DELETE FROM Page")
+    suspend fun deleteAll()
+}
