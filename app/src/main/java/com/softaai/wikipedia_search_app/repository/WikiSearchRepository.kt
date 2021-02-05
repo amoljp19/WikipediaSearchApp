2e8896fe43380fa1844a8fd62375b68657d0d152
amol pawar <amoljp19@gmail.com>
2021-02-05 15:08:44 +0530

    passes wikipedia api client in repository as parameter

--- app/src/main/java/com/softaai/wikipedia_search_app/repository/WikiSearchRepository.kt
+++ app/src/main/java/com/softaai/wikipedia_search_app/repository/WikiSearchRepository.kt
@@ -2,10 +2,11 @@ package com.softaai.wikipedia_search_app.repository
 
 import androidx.annotation.WorkerThread
 import com.softaai.wikipedia_search_app.model.Page
+import com.softaai.wikipedia_search_app.network.WikipediaApiClient
 import com.softaai.wikipedia_search_app.persistence.WikiSearchResponseDao
 import kotlinx.coroutines.flow.Flow
 
-class WikiSearchRepository(private val wikiSearchResponseDao: WikiSearchResponseDao) {
+class WikiSearchRepository(private val wikipediaApiClient: WikipediaApiClient, private val wikiSearchResponseDao: WikiSearchResponseDao) {
 
 
     val allUsers: Flow<List<Page>> = wikiSearchResponseDao.getAllWikiPages()
