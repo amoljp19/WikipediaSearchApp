c0e523c6ffaf347dd964d5265437e4e3cdce4fa9
amol pawar <amoljp19@gmail.com>
2021-02-05 11:25:36 +0530

    wrapped wiki pages data into coroutine flow

--- app/src/main/java/com/softaai/wikipedia_search_app/persistence/WikiSearchResponseDao.kt
+++ app/src/main/java/com/softaai/wikipedia_search_app/persistence/WikiSearchResponseDao.kt
@@ -5,12 +5,13 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.softaai.wikipedia_search_app.model.Page
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface WikiSearchResponseDao {
-    
+
     @Query("SELECT * FROM Page")
-    fun getAllWikiPages(): List<Page>
+    fun getAllWikiPages(): Flow<List<Page>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(page: Page)
