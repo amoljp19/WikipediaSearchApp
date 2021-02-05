3dc1f1f99c78337af19ba05983f6ebbd3e3ff2f0
amol pawar <amoljp19@gmail.com>
2021-02-05 16:04:57 +0530

    added recyclerview inflating and initializing code in main activity

--- app/src/main/java/com/softaai/wikipedia_search_app/wikipediasearch/ui/MainActivity.kt
+++ app/src/main/java/com/softaai/wikipedia_search_app/wikipediasearch/ui/MainActivity.kt
@@ -2,11 +2,19 @@ package com.softaai.wikipedia_search_app.wikipediasearch.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.softaai.wikipedia_search_app.R
+import com.softaai.wikipedia_search_app.wikipediasearch.adapter.WikiSearchListAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WikiSearchListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
